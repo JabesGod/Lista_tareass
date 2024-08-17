c378b086a77fc2165df51babc56cfec8174e53eb
JabesGod <99416529+JabesGod@users.noreply.github.com>
2024-08-16 22:49:42 -0500

    urls.py
    update

--- project/urls.py
+++ project/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('lista_tareas/', views.lista_tareas, name='lista_tareas'),
     path('agregar/', views.agregar_tarea, name='agregar_tarea'),
+    path('editar/<int:tarea_id>/', views.editar_tarea, name='editar_tarea'),
+    path('eliminar/<int:tarea_id>/', views.eliminar_tarea, name='eliminar_tarea'),
 
 ]
