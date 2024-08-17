455a9b3520e7d3a1e1f9be6069ba6a3bc7d134e5
JabesGod <99416529+JabesGod@users.noreply.github.com>
2024-08-16 22:49:11 -0500

    update editar y eliminaar
    :pig:

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Tarea
+from django.shortcuts import get_object_or_404
 
 def lista_tareas(request):
     tareas = Tarea.objects.all()
@@ -11,4 +12,22 @@ def agregar_tarea(request):
         if titulo:
             Tarea.objects.create(titulo=titulo)
         return redirect('lista_tareas')
-    return render(request, 'agregar_tarea.html')
+    return render(request, 'agregar_tarea.html') 
+
+def editar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    if request.method == "POST":
+        nuevo_titulo = request.POST.get('titulo')
+        if nuevo_titulo:
+            tarea.titulo = nuevo_titulo
+            tarea.save()
+        return redirect('lista_tareas')
+    return render(request, 'editar_tarea.html', {'tarea': tarea})
+
+
+def eliminar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    if request.method == "POST":
+        tarea.delete()
+        return redirect('lista_tareas')
+    return render(request, 'eliminar_tarea.html', {'tarea': tarea})
